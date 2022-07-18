f55392e65da560afe0047847b022ec55be9f8252
rbujny <radoslaw.bujny@gmail.com>
2022-07-18 16:20:43 +0200

    First changes

--- src/Controller/DeafultController.php
+++ src/Controller/DeafultController.php
@@ -3,15 +3,23 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DeafultController extends AbstractController
 {
     /**
-     * @Route("/")
+     * @Route("/", name="app_homepage")
      */
-    public function showIndex()
+    public function homepage(): Response
     {
-        return $this->render("base.html.twig");
+        return $this->render("homepage/homepage.html.twig");
+    }
+    /**
+     * @Route("/about", name="app_about")
+     */
+    public function aboutPage(): Response
+    {
+        return $this->render("homepage/about.html.twig");
     }
 }
